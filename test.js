d1326b971c4a94f3d4e96a70659c4ee962fa1de8
Sindre Sorhus <sindresorhus@gmail.com>
2017-01-10 12:02:07 +0700

    Init

---
+++ test.js
@@ -0,0 +1,22 @@
+import test from 'ava';
+import m from './';
+
+test(t => {
+	t.is(m('{foo}', {foo: '!'}), '!');
+	t.is(m('{foo}', {foo: 10}), '10');
+	t.is(m('{foo}{foo}', {foo: '!'}), '!!');
+	t.is(m('{foo}{bar}{foo}', {foo: '!', bar: '#'}), '!#!');
+	t.is(m('yo {foo} lol {bar} sup', {foo: '🦄', bar: '🌈'}), 'yo 🦄 lol 🌈 sup');
+
+	t.is(m('{foo}{deeply.nested.value}', {
+		foo: '!',
+		deeply: {
+			nested: {
+				value: '#'
+			}
+		}
+	}), '!#');
+
+	t.is(m('{0}{1}', ['!', '#']), '!#');
+});
+
