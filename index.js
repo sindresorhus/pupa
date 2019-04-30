5c8c3bcd743e6b61adeab61caac94cbd0f74a1e2
Aman Pratap Singh <amanprtpsingh@gmail.com>
2019-04-30 19:05:36 +0530

    Encode HTML entities inside placeholder when using double braces (#8)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const escapeGoat = require('escape-goat');
+
 module.exports = (template, data) => {
 	if (typeof template !== 'string') {
 		throw new TypeError(`Expected a string in the first argument, got ${typeof template}`);
@@ -7,6 +9,22 @@ module.exports = (template, data) => {
 
 	if (typeof data !== 'object') {
 		throw new TypeError(`Expected an Object/Array in the second argument, got ${typeof data}`);
+	}
+
+	const HTMLregex = /{{(.*?)}}/g;
+
+	if (HTMLregex.test(template)) {
+		template = template.replace(HTMLregex, (_, key) => {
+			let ret = data;
+			for (const prop of key.split('.')) {
+				ret = ret ? ret[prop] : '';
+			}
+
+			// Encoding HTML Entities to avoid code injection
+			ret = escapeGoat.escape(ret.toString());
+
+			return ret || '';
+		});
 	}
 
 	const regex = /{(.*?)}/g;
--- test.js
+++ test.js
@@ -2,6 +2,7 @@ import test from 'ava';
 import pupa from '.';
 
 test('main', t => {
+	// Normal placeholder
 	t.is(pupa('{foo}', {foo: '!'}), '!');
 	t.is(pupa('{foo}', {foo: 10}), '10');
 	t.is(pupa('{foo}{foo}', {foo: '!'}), '!!');
@@ -18,5 +19,25 @@ test('main', t => {
 	}), '!#');
 
 	t.is(pupa('{0}{1}', ['!', '#']), '!#');
+
+	// Encoding HTML Entities to avoid code injection
+	t.is(pupa('{{foo}}', {foo: '!'}), '!');
+	t.is(pupa('{{foo}}', {foo: 10}), '10');
+	t.is(pupa('{{foo}}{{foo}}', {foo: '!'}), '!!');
+	t.is(pupa('{foo}{{bar}}{foo}', {foo: '!', bar: '#'}), '!#!');
+	t.is(pupa('yo {{foo}} lol {{bar}} sup', {foo: '🦄', bar: '🌈'}), 'yo 🦄 lol 🌈 sup');
+
+	t.is(pupa('{foo}{{deeply.nested.value}}', {
+		foo: '!',
+		deeply: {
+			nested: {
+				value: '<br>#</br>'
+			}
+		}
+	}), '!&lt;br&gt;#&lt;/br&gt;');
+
+	t.is(pupa('{{0}}{{1}}', ['!', '#']), '!#');
+
+	t.is(pupa('{{0}}{{1}}', ['<br>yo</br>', '<i>lol</i>']), '&lt;br&gt;yo&lt;/br&gt;&lt;i&gt;lol&lt;/i&gt;');
 });
 
